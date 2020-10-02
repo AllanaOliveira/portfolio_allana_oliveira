a377f728a03eb4d5e5a6525c73d7562d1cabaf74
Allana Oliveira <allanaoliveira@id.uff.br>
2020-10-02 18:49:01 -0300

    Arquivos de configuração

--- projeto/wsgi.py
+++ projeto/wsgi.py
@@ -14,3 +14,22 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projeto.settings')
 
 application = get_wsgi_application()
+
+import os
+
+import sys
+
+## assumindo que sua aplicação django está em '/home/UFFCarlosRibeiro/projeto/projeto/prod.py'
+
+## e que seu manage.py está em '/home/UFFCarlosRibeiro/projeto/manage.py'
+
+path = 'D:\Allan\Documents\Projetos Django\portfolio\portfolio\projeto'
+if path not in sys.path:
+
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'produto.prod'
+
+from django.core.wsgi import get_wsgi_application
+
+application = get_wsgi_application()
